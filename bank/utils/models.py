bcb39b57e5ff57b3c157fef95287ce9fa54cd4d3
Torham <TorhamDev@gmail.com>
2023-08-16 07:32:09 +0330

    Add BaseModel

--- bank/utils/models.py
+++ bank/utils/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(_("date joined"), auto_now_add=True)
+    update_at = models.DateTimeField(_("update date"), auto_now=True)
+
+    class Meta:
+        abstract = True
