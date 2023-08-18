fa55540f3637ea4e9e8caf971f9e62dfdaa963c3
Torham <TorhamDev@gmail.com>
2023-08-18 18:04:08 +0330

    Fix Create superuser error

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 # TODO
 - [X] Update account model and add admin fields
-- [ ] Fix Create superuser error
+- [X] Fix Create superuser error
+- [ ] Add accounts model to panel admin
 - [ ] create register API
 - [ ] create auth API
 - [ ] Create wallet model for users
--- bank/user/manager.py
+++ bank/user/manager.py
@@ -6,27 +6,27 @@ class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(self, phone_number, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         """
         Creates and saves a User with the given phone number and password.
         """
-        if not phone_number:
-            raise ValueError("The given phone number must be set")
+        if not email:
+            raise ValueError("The given email must be set")
 
-        user = self.model(phone_number=phone_number, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, phone_number, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(phone_number, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, phone_number, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_staff", True)
 
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(phone_number, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
